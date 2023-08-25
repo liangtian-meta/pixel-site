cbb0636676b89813a3253387e3fbcf1b6be6a59a
liangtian-meta <liangtian@meta.com>
2023-08-24 17:49:23 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         facebookPixel: {
-          pixelId: "270030241538729",
+          pixelId: "1366712170917371",
           cookieName: "cookie-consent",
         },
         environments: ["production", "development"],
